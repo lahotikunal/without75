31b69aea3cf1e454a80e1890f3c908de0afa4c90
lahotikunal <37633212+lahotikunal@users.noreply.github.com>
2020-10-28 16:45:18 +0530

    Update lucky7java.js

--- lucky7java.js
+++ lucky7java.js
@@ -103,7 +103,8 @@ rules.addEventListener('click', function() {
 
 });
 next_game.addEventListener('click',function(){
-  {location.href = "https://lahotikunal.github.io/lucky-7/";}
+  if(potValue<0)
+        {location.href = "https://lahotikunal.github.io/lucky-7/";}
 });
 //rules
 
@@ -151,7 +152,7 @@ button.addEventListener('click',function()
         }
   if(potValue<0){
     
-    alert("game over!! not enough money left in the pot, hit refresh to play again")
+    alert("game over!! not enough money left in the pot, please press next game button")
   }
   document.getElementById("pot").innerHTML = potValue;
      
